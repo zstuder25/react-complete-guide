3d8a4c8d174410f15f668580469c4bc4aaf15686
Zachery Studer <zachery.j.studer@accenture.com>
2020-07-01 11:41:36 -0400

    section 5 part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+// import Radium, { StyleRoot } from "radium";
+// import styled from "styled-components";
 import './App.css';
 import Person from "./Person/Person";
+
+// const StyledButton = styled.button`
+//     background-color: ${props => props.alt ? 'red' : 'green'};
+//     color: white;
+//     font: inherit;
+//     border: 1px solid blue;
+//     padding: 8px;
+//     cursor: pointer;
+//
+//     &:hover {
+//       background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+//       color: black;
+//     }
+// `;
 
 class App extends Component {
   state = {
@@ -40,14 +56,18 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      // Limited to component but does not have full CSS functionality
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
+    // const style = {
+    //   // Limited to component but does not have full CSS functionality
+    //   backgroundColor: 'green',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // };
 
     let persons = null;
     if(this.state.showPersons){
@@ -62,22 +82,42 @@ class App extends Component {
               changed={(event) => this.nameChangeHandler(event, person.id)}
             />
           })}
-        </div>
+        </div>;
+
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //     color: 'black'
+      // }
     }
 
+    const classes = [];
+    if(this.state.persons.length <= 2 ){
+      classes.push('red')
+    }
+    if(this.state.persons.length <= 1 ){
+      classes.push('bold')
+    }
+
+
     return (
-      <div className="App">
-        <h1>Hi I'm a react app</h1>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Switch Name
-        </button>
-        {persons}
-      </div>
+      // <StyleRoot> // Radium styling for media queries
+        <div className="App">
+          <h1>Hi I'm a react app</h1>
+          <p className={classes.join(' ')}>This is really working</p>
+          <button
+            // alt={this.state.showPersons} // For Styled Components
+            onClick={this.togglePersonsHandler}>
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      // </StyleRoot>
     );
     // What this will compile to
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m a react app'))
   }
 }
 
-export default App;
+// export default Radium(App);
+export default App
